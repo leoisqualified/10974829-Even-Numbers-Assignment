7783a6240792d48ca194df8a598a355b3e35a217
Adenutsi Andy Eleos <102770998+leoisqualified@users.noreply.github.com>
2022-08-07 19:15:32 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /* average of even numbers less than 10000*/
+    for(int i=1;i<=10000;i++)
+    {
+    if(i%2==0)
+    cout<<i<<endl;
+    }
+}
+
