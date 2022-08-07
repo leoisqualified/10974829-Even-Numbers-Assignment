607721c6020a3d9519d292d31169617c6a187903
Adenutsi Andy Eleos <102770998+leoisqualified@users.noreply.github.com>
2022-08-07 21:55:24 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
     /* average of even numbers less than 10000*/
+    /*code created by Adenutsi Andy Eleos*/
     for(int i=2;i<=10000;i+=2)
     {
      sum +=i;
